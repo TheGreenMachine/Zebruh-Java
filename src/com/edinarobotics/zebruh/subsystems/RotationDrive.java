88cdb6c475344b242d99a277ca0c8489accc5ea5
Eric <congpeng89@gmail.com>
2015-01-07 20:08:34 -0600

    Added RotationDrive subsystem.
    Susbsystem for the Hdrive drivetrain.

---
+++ src/com/edinarobotics/zebruh/subsystems/RotationDrive.java
@@ -0,0 +1,36 @@
+package com.edinarobotics.zebruh.subsystems;
+
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RotationDrive extends Subsystem1816 {
+	private Drivetrain drivetrain;
+	private double rotation;
+
+	public RotationDrive(Drivetrain drivetrain) {
+		this.drivetrain = drivetrain;
+	}
+
+	public double getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(double rotation) {
+		this.rotation = rotation;
+		update();
+	}
+
+	@Override
+	public void update() {
+		drivetrain.setRotation(rotation);
+	}
+	
+	public void setDefaultCommand(Command command){
+        if(getDefaultCommand() != null){
+            super.getDefaultCommand().cancel();
+        }
+        super.setDefaultCommand(command);
+    }
+
+}
