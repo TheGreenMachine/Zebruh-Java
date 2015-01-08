57b50c4f1fa23626851b0d6e5cd9b78b2cd12577
Eric <congpeng89@gmail.com>
2015-01-07 20:02:23 -0600

    Added Controls class.
    This class is a singleton class that will control the gamepads.

---
+++ src/com/edinarobotics/zebruh/Controls.java
@@ -0,0 +1,38 @@
+package com.edinarobotics.zebruh;
+
+import java.util.Vector;
+
+import com.edinarobotics.utils.gamepad.FilteredGamepad;
+import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.utils.gamepad.gamepadfilters.DeadzoneFilter;
+import com.edinarobotics.utils.gamepad.gamepadfilters.GamepadFilterSet;
+import com.edinarobotics.utils.gamepad.gamepadfilters.PowerFilter;
+
+public class Controls {
+	private static Controls instance;
+	
+	public final Gamepad gamepad;
+	
+	 private Controls() {        
+	        Vector gamepadFilters = new Vector();
+	        gamepadFilters.addElement(new DeadzoneFilter(0.1));
+	        gamepadFilters.addElement(new PowerFilter(2));
+	        GamepadFilterSet shootGamepadFilterSet = new GamepadFilterSet(gamepadFilters);
+	        gamepad = new FilteredGamepad(1, shootGamepadFilterSet);
+	        
+	    }
+	
+	 /**
+     * Returns the proper instance of Controls. This method creates a new
+     * Controls object the first time it is called and returns that object for
+     * each subsequent call.
+     *
+     * @return The current instance of Controls.
+     */
+    public static Controls getInstance() {
+        if (instance == null) {
+            instance = new Controls();
+        }
+        return instance;
+    }
+}
