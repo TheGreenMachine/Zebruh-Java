16eabd335722bb3f91eb27af0029e63497166195
Eric <congpeng89@gmail.com>
2015-01-07 20:02:50 -0600

    Added Components class.
    This is a singleton class that will encompass all subsystems of the robot.

--- src/com/edinarobotics/zebruh/Components.java
+++ src/com/edinarobotics/zebruh/Components.java
@@ -1,34 +1,48 @@
 package com.edinarobotics.zebruh;
 
 import com.edinarobotics.zebruh.subsystems.Drivetrain;
+import com.edinarobotics.zebruh.subsystems.HorizontalStrafe;
+import com.edinarobotics.zebruh.subsystems.RotationDrive;
+import com.edinarobotics.zebruh.subsystems.VerticalStrafe;
 
 public class Components {
 	private static Components instance;
-	
+
 	public Drivetrain drivetrain;
-	
-	//PWM Constants
-		//Drivetrain Constants
+	public RotationDrive rotationDrive;
+	public HorizontalStrafe horizontalStrafe;
+	public VerticalStrafe verticalStrafe;
+
+	// PWM Constants
+		// Drivetrain Constants
 		private static final int TOP_LEFT_CANTALON = 1;
 		private static final int TOP_RIGHT_CANTALON = 1;
 		private static final int BOTTOM_LEFT_CANTALON = 1;
 		private static final int BOTTOM_RIGHT_CANTALON = 1;
 		private static final int MIDDLE_TOP_CANTALON = 1;
 		private static final int MIDDLE_BOTTOM_CANTALON = 1;
-		//End Drivetrain Constants
-	//End PWM Constants
-	
-	
-	
+		// End Drivetrain Constants
+	// End PWM Constants
+
+	private Components() {
+		this.drivetrain = new Drivetrain(TOP_LEFT_CANTALON, TOP_RIGHT_CANTALON,
+				BOTTOM_LEFT_CANTALON, BOTTOM_RIGHT_CANTALON,
+				MIDDLE_TOP_CANTALON, MIDDLE_BOTTOM_CANTALON);
+		this.rotationDrive = new RotationDrive(drivetrain);
+		this.horizontalStrafe = new HorizontalStrafe(drivetrain);
+		this.verticalStrafe = new VerticalStrafe(drivetrain);
+	}
+
 	/**
-     * Returns a new instance of {@link Components}, creating one if null.
-     * @return {@link Components}
-     */
-    public static Components getInstance() {
-        if(instance == null){
-            instance = new Components();
-        }
-        return instance;
-    }
+	 * Returns a new instance of {@link Components}, creating one if null.
+	 * 
+	 * @return {@link Components}
+	 */
+	public static Components getInstance() {
+		if (instance == null) {
+			instance = new Components();
+		}
+		return instance;
+	}
 
 }
