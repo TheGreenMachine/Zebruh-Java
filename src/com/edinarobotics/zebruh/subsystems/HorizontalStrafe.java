2322c16b7bc4e9dd682bc4a45a8cae1f2e5bb6f1
Eric <congpeng89@gmail.com>
2015-01-07 20:08:17 -0600

    Added HorizontalStrafe subsystem.
    Susbsystem for the Hdrive drivetrain.

---
+++ src/com/edinarobotics/zebruh/subsystems/HorizontalStrafe.java
@@ -0,0 +1,38 @@
+package com.edinarobotics.zebruh.subsystems;
+
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+
+public class HorizontalStrafe extends Subsystem1816 {
+	private Drivetrain drivetrain;
+	
+	private double horizontalStrafe;
+	
+	public HorizontalStrafe(Drivetrain drivetrain) {
+		this.drivetrain = drivetrain;
+	}
+
+	public void setHorizontalStrafe(double horizontalStrafe) {
+		this.horizontalStrafe = horizontalStrafe;
+		update();
+	}
+	
+	public double getHoritzonalStrafe() {
+		return horizontalStrafe;
+	}
+	
+	@Override
+	public void update() {
+		drivetrain.setHorizontalStrafe(horizontalStrafe);
+	}
+	
+	public void setDefaultCommand(Command command){
+        if(getDefaultCommand() != null){
+            super.getDefaultCommand().cancel();
+        }
+        super.setDefaultCommand(command);
+    }
+
+}
