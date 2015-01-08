afc49d6ac094d0cfdb5b1deb0bce852a08ea1ed4
Eric <congpeng89@gmail.com>
2015-01-07 20:05:48 -0600

    Added GamepadRotationCommand.
    This will get the y axis value of the right joystick.

---
+++ src/com/edinarobotics/zebruh/commands/GamepadRotationCommand.java
@@ -0,0 +1,51 @@
+package com.edinarobotics.zebruh.commands;
+
+import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.zebruh.Components;
+import com.edinarobotics.zebruh.subsystems.RotationDrive;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GamepadRotationCommand extends Command {
+
+	private RotationDrive rotationDrive;
+	private Gamepad gamepad;
+	
+	public GamepadRotationCommand(Gamepad gamepad){
+		super("GamepadRotationCommand");
+		this.gamepad = gamepad;
+		this.rotationDrive = Components.getInstance().rotationDrive;
+	}
+	
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		double rotation = gamepad.getGamepadAxisState().getRightJoystick().getX();
+		rotationDrive.setRotation(rotation);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
