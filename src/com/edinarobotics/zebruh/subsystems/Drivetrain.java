e8f2e1168a31d105a4a2db5d5955e65c5db9731a
Eric <congpeng89@gmail.com>
2015-01-07 20:07:11 -0600

    Added Drivetrain subsystem.
    This uses the custom RobotDrive for this year's drivetrain.

--- src/com/edinarobotics/zebruh/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zebruh/subsystems/Drivetrain.java
@@ -1,13 +1,19 @@
 package com.edinarobotics.zebruh.subsystems;
 
-import com.edinarobotics.utils.subsystems.Subsystem1816;
+import com.edinarobotics.utils.common.Updatable;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class Drivetrain extends Subsystem1816 {
-	
-	private CANTalon topLeft, topRight, bottomLeft, bottomRight, middleTop, middleBottom;
-	
+public class Drivetrain implements Updatable {
+
+	private CANTalon topLeft, topRight, bottomLeft, bottomRight, middleTop,
+			middleBottom;
+
+	private double verticalStrafe, horizontalStrafe, rotation;
+
+	private HRobotDrive hRobotDrive;
+
 	public Drivetrain(int topLeft, int topRight, int bottomLeft,
 			int bottomRight, int middleTop, int middleBottom) {
 		this.topLeft = new CANTalon(topLeft);
@@ -16,11 +22,28 @@ public class Drivetrain extends Subsystem1816 {
 		this.bottomRight = new CANTalon(bottomRight);
 		this.middleTop = new CANTalon(middleTop);
 		this.middleBottom = new CANTalon(middleBottom);
+		hRobotDrive = new HRobotDrive(topLeft, bottomLeft, topRight,
+				bottomRight, middleBottom, middleTop);
+	}
+
+	public void setVerticalStrafe(double verticalStrafe) {
+		this.verticalStrafe = verticalStrafe;
+		update();
+	}
+
+	public void setHorizontalStrafe(double horizontalStrafe) {
+		this.horizontalStrafe = horizontalStrafe;
+		update();
+	}
+	
+	public void setRotation (double setRotation) {
+		this.rotation = setRotation;
+		update();
 	}
 
 	@Override
 	public void update() {
-
+		hRobotDrive.hDrive(verticalStrafe, horizontalStrafe, rotation);
 	}
 
 }
