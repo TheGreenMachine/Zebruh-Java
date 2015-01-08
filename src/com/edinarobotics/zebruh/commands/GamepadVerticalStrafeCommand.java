61b84040b6a075554fd5052b732bf6c55b136f82
Eric <congpeng89@gmail.com>
2015-01-07 20:06:27 -0600

    Added GamepadVerticalStrafeCommand.
    This will get the y axis value from the left joystick.

---
+++ src/com/edinarobotics/zebruh/commands/GamepadVerticalStrafeCommand.java
@@ -0,0 +1,45 @@
+package com.edinarobotics.zebruh.commands;
+
+import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.zebruh.Components;
+import com.edinarobotics.zebruh.subsystems.VerticalStrafe;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GamepadVerticalStrafeCommand extends Command {
+	private Gamepad gamepad;
+	private VerticalStrafe verticalStrafe;
+
+	public GamepadVerticalStrafeCommand(Gamepad gamepad) {
+		super("GamepadVerticalStrafe");
+		this.gamepad = gamepad;
+		verticalStrafe = Components.getInstance().verticalStrafe;
+	}
+
+	@Override
+	protected void initialize() {
+
+	}
+
+	@Override
+	protected void execute() {
+		double verticalStrafe = gamepad.getGamepadAxisState().getLeftJoystick().getY();
+		this.verticalStrafe.setVerticalStrafe(verticalStrafe);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+
+	}
+
+	@Override
+	protected void interrupted() {
+
+	}
+
+}
