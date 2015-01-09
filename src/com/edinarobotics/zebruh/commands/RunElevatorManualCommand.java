68b2a096b575020a08a9a007e95dd9e08b54acc6
Eric <congpeng89@gmail.com>
2015-01-09 14:38:53 -0600

    Added RunEelevatorManualCommand.
    This will run the elevator manually.

---
+++ src/com/edinarobotics/zebruh/commands/RunElevatorManualCommand.java
@@ -0,0 +1,48 @@
+package com.edinarobotics.zebruh.commands;
+
+import com.edinarobotics.zebruh.Components;
+import com.edinarobotics.zebruh.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RunElevatorManualCommand extends Command{
+	
+	private Elevator elevator;
+	private boolean isOverride;
+	
+	private double elevatorSpeed;
+	
+	public RunElevatorManualCommand(Elevator elevator, boolean isOverride) {
+		super("RunElevatorManual");
+		this.elevator = Components.getInstance().elevator;
+		this.elevator = elevator;
+		this.isOverride = isOverride;
+		requires(elevator);
+	}
+
+	@Override
+	protected void initialize() {
+		
+	}
+
+	@Override
+	protected void execute() {
+		elevator.setElevatorSpeed(elevatorSpeed, isOverride);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+}
