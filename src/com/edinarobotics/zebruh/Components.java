1752f0f6559902c35bcb372fb166e151c63ef8e6
Eric <congpeng89@gmail.com>
2015-01-16 09:28:38 -0600

    Added subsystem compnents IDs.
    Elevator limit switches and for Claw subsystem.

--- src/com/edinarobotics/zebruh/Components.java
+++ src/com/edinarobotics/zebruh/Components.java
@@ -1,5 +1,6 @@
 package com.edinarobotics.zebruh;
 
+import com.edinarobotics.zebruh.subsystems.Claw;
 import com.edinarobotics.zebruh.subsystems.Drivetrain;
 import com.edinarobotics.zebruh.subsystems.Elevator;
 import com.edinarobotics.zebruh.subsystems.HorizontalStrafe;
@@ -14,22 +15,36 @@ public class Components {
 	public HorizontalStrafe horizontalStrafe;
 	public VerticalStrafe verticalStrafe;
 	public Elevator elevator;
+	public Claw claw;
 	
 	// PWM Constants
 		// Drivetrain Constants
-		private static final int TOP_LEFT_CANTALON = 1;
-		private static final int TOP_RIGHT_CANTALON = 1;
-		private static final int BOTTOM_LEFT_CANTALON = 1;
+		private static final int TOP_LEFT_CANTALON = 4;
+		private static final int TOP_RIGHT_CANTALON = 6;
+		private static final int BOTTOM_LEFT_CANTALON = 3;
 		private static final int BOTTOM_RIGHT_CANTALON = 1;
-		private static final int MIDDLE_TOP_CANTALON = 1;
-		private static final int MIDDLE_BOTTOM_CANTALON = 1;
+		private static final int MIDDLE_TOP_CANTALON = 5;
+		private static final int MIDDLE_BOTTOM_CANTALON = 2;
 		// End Drivetrain Constants
 		
 		//Elevator Constants
-		private static final int ELEVATOR_TALON = 1;
-		private static final int ELEVATOR_ENCODER_CHANNEL_A = 1;
-		private static final int ELEVATOR_ENCODER_CHANNEL_B = 1;
+		private static final int ELEVATOR_CANTALON1 = 12;
+		private static final int ELEVATOR_CANTALON2 = 13;
+		private static final int ELEVATOR_ENCODER_CHANNEL_A = 14;
+		private static final int ELEVATOR_ENCODER_CHANNEL_B = 199;
 		//End Elevator Constant
+		
+		//Claw Constants
+		private static final int CLAW_SOLENOID_CHANNEL_A = 15;
+		private static final int CLAW_SOLENOID_CHANNEL_B = 16;
+		private static final int ROTATE_SOLENOID_CHANNEL_A = 17;
+		private static final int ROTATE_SOLENOID_CHANNEL_B = 18;
+		
+		
+		//DOI Constants
+		private static final int UPPER_ELEVATOR_SWTICH = 19;
+		private static final int LOWER_ELEVATOR_SWTICH = 100;
+		//End DOI Constants
 		
 	// End PWM Constants
 
@@ -40,7 +55,9 @@ public class Components {
 		this.rotationDrive = new RotationDrive(drivetrain);
 		this.horizontalStrafe = new HorizontalStrafe(drivetrain);
 		this.verticalStrafe = new VerticalStrafe(drivetrain);
-		this.elevator = new Elevator(ELEVATOR_TALON, ELEVATOR_ENCODER_CHANNEL_A, ELEVATOR_ENCODER_CHANNEL_B);
+		//this.elevator = new Elevator(ELEVATOR_CANTALON1, ELEVATOR_CANTALON2, ELEVATOR_ENCODER_CHANNEL_A,
+		//		ELEVATOR_ENCODER_CHANNEL_B, UPPER_ELEVATOR_SWTICH, LOWER_ELEVATOR_SWTICH);
+		//this.claw = new Claw(CLAW_SOLENOID_CHANNEL_A, CLAW_SOLENOID_CHANNEL_B, ROTATE_SOLENOID_CHANNEL_A, ROTATE_SOLENOID_CHANNEL_B);
 	}
 
 	/**
