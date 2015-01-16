5e695ecfca2e9ab80ac9d23e83e42d5292ba1deb
Eric <congpeng89@gmail.com>
2015-01-16 09:25:37 -0600

    Added Claw susbsystem.

---
+++ src/com/edinarobotics/zebruh/subsystems/Claw.java
@@ -0,0 +1,58 @@
+package com.edinarobotics.zebruh.subsystems;
+
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class Claw extends Subsystem1816 {
+
+	private DoubleSolenoid clampSolenoid;
+	private DoubleSolenoid rotateSolenoid;
+	private DoubleSolenoid.Value clampValue;
+	private DoubleSolenoid.Value rotateValue;
+	
+	public Claw(int clampChannel1, int clampChannel2, int rotateChannel1, int rotateChannel2) {
+		this.clampSolenoid = new DoubleSolenoid(clampChannel1,clampChannel2);
+		this.rotateSolenoid = new DoubleSolenoid(rotateChannel1,rotateChannel2);
+		
+	}
+	
+	public enum SolenoidState {
+		CLAMP_OPEN, CLAMP_CLOSE, ROTATE_UP, ROTATE_DOWN;
+	}
+	
+	public void setState(SolenoidState state){
+		switch(state) {
+			case CLAMP_OPEN:
+				if(getRotateState() == DoubleSolenoid.Value.kReverse) 
+					clampValue = DoubleSolenoid.Value.kForward;
+				break;
+			case CLAMP_CLOSE:
+				clampValue = DoubleSolenoid.Value.kReverse;
+				break;
+			case ROTATE_UP:
+				rotateValue = DoubleSolenoid.Value.kForward;
+				break;
+			case ROTATE_DOWN:
+				rotateValue = DoubleSolenoid.Value.kReverse;
+				break;
+		}
+	}
+	
+	public Value getClampState() {
+		return clampSolenoid.get();
+	}
+	
+	public Value getRotateState() {
+		return clampSolenoid.get();
+	}
+	
+	@Override
+	public void update() {
+		clampSolenoid.set(clampValue);		
+		rotateSolenoid.set(rotateValue);
+	}
+
+	
+}
