94466c68a5b5070475c61fd916554b41494d4486
Eric <congpeng89@gmail.com>
2015-01-24 12:07:36 -0600

    Added AutonomousCommand.
    Establishing threshold for our autonomous.

---
+++ src/com/edinarobotics/zebruh/commands/AutonomousCommand.java
@@ -0,0 +1,13 @@
+package com.edinarobotics.zebruh.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousCommand extends CommandGroup {
+	
+	public AutonomousCommand() {
+		
+	}
+		
+				
+	}
+
