cd98c2171602cad0ada1b6fa01e923d5d9375574
Eric <congpeng89@gmail.com>
2015-01-30 11:01:47 -0600

    Edited the constructor of elevator.
    The encoder is now hooked up to the CANTalon, rather than the roboRIO.

--- src/com/edinarobotics/zebruh/Components.java
+++ src/com/edinarobotics/zebruh/Components.java
@@ -19,19 +19,23 @@ public class Components {
 	
 	// PWM Constants
 		// Drivetrain Constants
-		private static final int TOP_LEFT_CANTALON = 4;
-		private static final int TOP_RIGHT_CANTALON = 6;
-		private static final int BOTTOM_LEFT_CANTALON = 3;
-		private static final int BOTTOM_RIGHT_CANTALON = 1;
-		private static final int MIDDLE_TOP_CANTALON = 5;
-		private static final int MIDDLE_BOTTOM_CANTALON = 2;
+		private static final int TOP_LEFT_CANTALON = 7;
+		private static final int TOP_RIGHT_CANTALON = 3;
+		private static final int BOTTOM_LEFT_CANTALON = 8;
+		private static final int BOTTOM_RIGHT_CANTALON = 4;
+		private static final int MIDDLE_TOP_CANTALON = 2;
+		private static final int MIDDLE_BOTTOM_CANTALON = 5;
 		// End Drivetrain Constants
 		
 		//Elevator Constants
-		private static final int ELEVATOR_CANTALON1 = 12;
-		private static final int ELEVATOR_CANTALON2 = 13;
-		private static final int ELEVATOR_ENCODER_CHANNEL_A = 14;
-		private static final int ELEVATOR_ENCODER_CHANNEL_B = 199;
+		private static final int ELEVATOR_CANTALON1 = 1;
+		private static final int ELEVATOR_CANTALON2 = 6;
+		
+		private static final int LIMIT_SWITCH_1 = 1;
+		private static final int LIMIT_SWITCH_2 = 2;
+		private static final int LIMIT_SWITCH_3 = 3;
+		private static final int LIMIT_SWITCH_4 = 4;
+		
 		//End Elevator Constant
 		
 		//Claw Constants
@@ -40,13 +44,13 @@ public class Components {
 		private static final int ROTATE_SOLENOID_CHANNEL_A = 17;
 		private static final int ROTATE_SOLENOID_CHANNEL_B = 18;
 		
-		
-		//DOI Constants
-		private static final int UPPER_ELEVATOR_SWTICH = 19;
-		private static final int LOWER_ELEVATOR_SWTICH = 100;
-		//End DOI Constants
-		
 	// End PWM Constants
+		
+	// Elevator PID Constants
+		private static final double P = 0.15;
+		private static final double I = 0.00001;
+		private static final double D = 3;
+	// End Elevator PID Constants
 
 	private Components() {
 		this.drivetrain = new Drivetrain(TOP_LEFT_CANTALON, TOP_RIGHT_CANTALON,
@@ -55,8 +59,8 @@ public class Components {
 		this.rotationDrive = new RotationDrive(drivetrain);
 		this.horizontalStrafe = new HorizontalStrafe(drivetrain);
 		this.verticalStrafe = new VerticalStrafe(drivetrain);
-		//this.elevator = new Elevator(ELEVATOR_CANTALON1, ELEVATOR_CANTALON2, ELEVATOR_ENCODER_CHANNEL_A,
-		//		ELEVATOR_ENCODER_CHANNEL_B, UPPER_ELEVATOR_SWTICH, LOWER_ELEVATOR_SWTICH);
+		this.elevator = new Elevator(ELEVATOR_CANTALON1, ELEVATOR_CANTALON2, P, I, D, 
+				LIMIT_SWITCH_1, LIMIT_SWITCH_2, LIMIT_SWITCH_3, LIMIT_SWITCH_4);
 		//this.claw = new Claw(CLAW_SOLENOID_CHANNEL_A, CLAW_SOLENOID_CHANNEL_B, ROTATE_SOLENOID_CHANNEL_A, ROTATE_SOLENOID_CHANNEL_B);
 	}
 
--- src/com/edinarobotics/zebruh/subsystems/Elevator.java
+++ src/com/edinarobotics/zebruh/subsystems/Elevator.java
@@ -3,102 +3,124 @@ package com.edinarobotics.zebruh.subsystems;
 import com.edinarobotics.utils.subsystems.Subsystem1816;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.ControlMode;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
+import edu.wpi.first.wpilibj.command.Command;
 
 public class Elevator extends Subsystem1816 {
 
-	public CANTalon leftTalon, rightTalon;
-	public DigitalInput upperLimit, lowerLimit;
-	public Encoder elevatorEncoder; 
-	public ElevatorLevel targetLevel;
+	private CANTalon talonA, talonB;
+	private DigitalInput ls1, ls2, ls3, ls4;
+	private ElevatorLevel targetLevel;
 	
 	private double teleOpElevatorSpeed = 1.0;
-	private double elevatorSpeed = 1.0;
-	private boolean isOverride = false;
-	private boolean hasBeenOverridden = false;
-	private boolean isCalibrated = false;
 	
-	public final int ELEVATOR_DEADZONE_TICKS = 666;
+	private boolean override;
+		
+	private double p, i, d;
 			
-	public Elevator(int leftTalon, int rightTalon, int upperLimit, int lowerLimit, 
-			int channelA, int channelB) {
-		this.leftTalon = new CANTalon(leftTalon);
-		this.rightTalon = new CANTalon(rightTalon);
-		this.leftTalon.changeControlMode(CANTalon.ControlMode.Position);
-		this.upperLimit = new DigitalInput(upperLimit);
-		this.lowerLimit = new DigitalInput(lowerLimit);
+	public Elevator(int leftTalon, int rightTalon, double p, double i, double d, 
+			int ls1, int ls2, int ls3, int ls4) {
+		talonA = new CANTalon(leftTalon);
+		talonB = new CANTalon(rightTalon);
+		talonA.enableControl();
+		talonB.enableControl();
+		talonA.changeControlMode(CANTalon.ControlMode.Position);
+		talonA.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		talonA.setPID(p, i, d);
+		this.p = p;
+		this.i = i;
+		this.d = d;
+		this.ls1 = new DigitalInput(ls1);
+		this.ls2 = new DigitalInput(ls2);
+		this.ls3 = new DigitalInput(ls3);
+		this.ls4 = new DigitalInput(ls4);
+		targetLevel = Elevator.ElevatorLevel.A;
+		override = false;
+		setPosition(0);
 	}
 	
 	public void setElevatorLevel(ElevatorLevel level) {
-		hasBeenOverridden = false;
-		isOverride = false;
+		setOverride(false);
 		targetLevel = level;
-		leftTalon.setPosition(targetLevel.encoderTicks);
-		rightTalon.setPosition(targetLevel.encoderTicks);
 		update();
 	}
 	
-	public void setOverride(boolean isOverride) {
-		this.isOverride = isOverride;
-		update();
+	public int getEncoderTicks() {
+		return talonA.getEncPosition();
 	}
 	
-	public void setTeleOpElevatorSpeed(double speed){
+	public void setElevatorSpeed(double speed) {
+		setOverride(true);
 		teleOpElevatorSpeed = speed;
 		update();
 	}
 	
-	public enum ApproxPosition {
-		PUSH_BIN(0),
-		PUSH_TOTE(1),
-		PUSH_BIN_TOTE(2),
-		LANDFILL_AUTO(3);
-		
-		private int positionTicks;
-		
-		private ApproxPosition(int positionTicks) {
-			this.positionTicks = positionTicks;
+	public Elevator.ElevatorLevel getTargetLevel() {
+		return targetLevel;
+	}
+	
+	public void setOverride(boolean override) {
+		talonA.changeControlMode(override ? ControlMode.PercentVbus : ControlMode.Position);
+		if(!override) {
+			talonA.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+			talonA.setPID(p, i, d);
 		}
+		this.override = override;
+	}
+	
+	public void setPosition(int position) {
+		talonA.setPosition(position);
 	}
 	
 	public enum ElevatorLevel {
-		A(0),
-		B(1),
-		C(2),
-		D(3),
-		E(4),
-		F(5);
+		A(4000),
+		B(8000),
+		C(12000),
+		D(16000),
+		E(20000),
+		F(24000),
+		G(0);
 		
 		private int encoderTicks;
+		
+		public int getEncoderTicks() {
+			return encoderTicks;
+		}
 		
 		private ElevatorLevel(int encoderTicks) {
 			this.encoderTicks = encoderTicks;
 		}
 	}
 	
+	public CANTalon getTalon() {
+		return talonA;
+	}
+	
+	public boolean getLS1() {
+		return ls1.get();
+	}
+	
+	public boolean getLS2() {
+		return ls2.get();
+	}
+	
+	public boolean getLS3() {
+		return ls3.get();
+	}
+	
+	public boolean getLS4() {
+		return ls4.get();
+	}
+	
 	public void update() {
-		if(!isOverride && !hasBeenOverridden && isCalibrated){
-			if(elevatorEncoder.get() < targetLevel.encoderTicks) {
-				leftTalon.set(elevatorSpeed);
-				rightTalon.set(elevatorSpeed);
-			} else if (elevatorEncoder.get() > targetLevel.encoderTicks + ELEVATOR_DEADZONE_TICKS){
-				leftTalon.set(-elevatorSpeed);
-				rightTalon.set(-elevatorSpeed);
-			} else {
-				leftTalon.set(0.0);
-				rightTalon.set(0.0);
-			}
-		} else if(isOverride) {
-			hasBeenOverridden = true;
-			leftTalon.set(teleOpElevatorSpeed);
-			rightTalon.set(teleOpElevatorSpeed);
-		} else if (hasBeenOverridden){
-			leftTalon.set(0);
-			rightTalon.set(0);
-		} else {
-			leftTalon.set(0);
-			rightTalon.set(0);
+		if(override) { //manual control mode
+			talonA.set(teleOpElevatorSpeed);
+			talonB.set(teleOpElevatorSpeed);
+		} else { //auto control mode
+			talonA.set(targetLevel.encoderTicks);
+			talonB.set(talonA.getOutputVoltage());
 		}
 	}
 }
